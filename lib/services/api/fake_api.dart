58b247afdbc732936edf55d7d76471f19ac4653f
samuelematias <hi@samuelematias.com>
2020-08-11 12:59:08 -0300

    feat(services): create the base services

---
+++ lib/services/api/fake_api.dart
@@ -0,0 +1,29 @@
+import 'package:abstraction_fake_data/datamodels/api_models.dart';
+
+import 'api.dart';
+
+class FakeApi implements Api {
+  @override
+  Future<List<Comment>> getCommentsForPost(int postId) {
+    // TODO: implement getCommentsForPost
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Post>> getPostsForUser(int userId) {
+    // TODO: implement getPostsForUser
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<User> getUser(int userId) {
+    // TODO: implement getUser
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<LoginResponse> login({String username, String password}) {
+    // TODO: implement login
+    throw UnimplementedError();
+  }
+}
