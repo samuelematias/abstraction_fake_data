4fd9ce9868d3198679d4ec20d58dac89d4269273
samuelematias <hi@samuelematias.com>
2020-08-11 12:58:45 -0300

    feat(datamodels): create Api models

---
+++ lib/datamodels/api_models.dart
@@ -0,0 +1,25 @@
+class Post {}
+
+class Comment {}
+
+class User {
+  final String name;
+  final String phoneNumber;
+
+  User({
+    this.name,
+    this.phoneNumber,
+  });
+}
+
+class LoginResponse {
+  final bool success;
+  final int userId;
+  final String message;
+
+  LoginResponse({
+    this.success = true,
+    this.userId,
+    this.message,
+  });
+}
