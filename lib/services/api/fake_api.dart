46156d56aed2c40b51c42d091e07923a96261a36
samuelematias <hi@samuelematias.com>
2020-08-11 13:29:17 -0300

    feat(api): create some FakeApi uses case

--- lib/services/api/fake_api.dart
+++ lib/services/api/fake_api.dart
@@ -4,26 +4,58 @@ import 'api.dart';
 
 class FakeApi implements Api {
   @override
-  Future<List<Comment>> getCommentsForPost(int postId) {
-    // TODO: implement getCommentsForPost
-    throw UnimplementedError();
+  Future<List<Comment>> getCommentsForPost(int postId) async {
+    await Future.delayed(Duration(seconds: 1));
+
+    if (postId == 1) {
+      return List<Comment>.generate(10, (index) => Comment());
+    }
+
+    return null;
   }
 
   @override
-  Future<List<Post>> getPostsForUser(int userId) {
-    // TODO: implement getPostsForUser
-    throw UnimplementedError();
+  Future<List<Post>> getPostsForUser(int userId) async {
+    await Future.delayed(Duration(seconds: 1));
+
+    if (userId == 1) {
+      return List<Post>.generate(10, (index) => Post());
+    }
+
+    if (userId == 2) {
+      return List<Post>();
+    }
+
+    return null;
   }
 
   @override
-  Future<User> getUser(int userId) {
-    // TODO: implement getUser
-    throw UnimplementedError();
+  Future<User> getUser(int userId) async {
+    await Future.delayed(Duration(seconds: 1));
+
+    if (userId == 1) {
+      return User(name: 'dane', phoneNumber: '999-999-00');
+    }
+
+    if (userId == 2) {
+      return User(name: 'Flutter', phoneNumber: '737-000-93');
+    }
+
+    return null;
   }
 
   @override
-  Future<LoginResponse> login({String username, String password}) {
-    // TODO: implement login
-    throw UnimplementedError();
+  Future<LoginResponse> login({String username, String password}) async {
+    await Future.delayed(Duration(seconds: 1));
+
+    if (username == 'dane') {
+      return LoginResponse(userId: 1);
+    }
+
+    if (username == 'flutter') {
+      return LoginResponse(userId: 2);
+    }
+
+    return LoginResponse(success: false, message: 'Username not found');
   }
 }
